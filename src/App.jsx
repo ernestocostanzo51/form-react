54bd165f5d32d79350e79ca209ac1e3b6ab227fc
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-03-12 15:08:55 +0100

    creazione form con input e btn

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,19 @@ function App() {
   return (
     <>
     <div className="container">
+      <div className="row text-center">
+        <div className="col m-4">
+          <h4>Inserisci un nuovo titolo</h4>
+          <form>
+            <input type="text"></input>
+            <button type="submit" className="btn-primary">Inserisci</button>
+          </form>
+        </div>
+      </div>
+    </div>
+
+
+    <div className="container">
       <h2 className="text-center">Titoli degli articoli</h2>
       <div className="row d-flex flex-column text-center">
         {
