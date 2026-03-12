f9b3871436381cdd3d83920bd21b921dbe88a723
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-03-12 15:03:29 +0100

    MILESTONE 1

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,18 @@ function App() {
 
   return (
     <>
-    
+    <div className="container">
+      <h2 className="text-center">Titoli degli articoli</h2>
+      <div className="row d-flex flex-column text-center">
+        {
+         articles.map(article =>(
+          <div className="col m-2 text-center">
+            {article}
+          </div>
+         ))
+        }
+      </div>
+    </div>
      
      </>
   )
